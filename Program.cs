6d3700699d9d3460cf9e717da65cd03602e3fb53
GabrielNP96 <gabrielnp1996@gmail.com>
2025-03-27 12:55:44 -0300

    Primeiro commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ConcessionariCLI.Models;
+
+abstract class user
+{
+    private static int _idCounter = 0;
+    public user(string name, int cpf, string position)
+    {
+        Id = _idCounter ++;
+        Name = name;
+        Cpf = cpf;
+        Position = position;
+    }
+
+    public int Id { get; }
+    public string Name { get; }
+    public int Cpf { get; }
+    public string Position { get; }
+
+}
