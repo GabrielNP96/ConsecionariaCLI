d04889c0bf356ee36fabf5415b1abd07ac67a269
GabrielNP96 <gabrielnp1996@gmail.com>
2025-03-28 12:58:12 -0300

    Criando a conexão

---
+++ Database/SqliteDatabase.cs
@@ -0,0 +1,13 @@
+using System.Data.SQLite;
+
+namespace ConcessionariaCLI.Database;
+
+public class SQLiteDataBase
+{
+    private string _conectionString = "Data Source=concessionaria.db;Version=3;";
+
+    public static SQLiteConnection Connect()
+    {
+        
+    }
+}
