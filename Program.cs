e3e5bd27c232716cf4e1122a5efaaa42f82be69a
GabrielNP96 <gabrielnp1996@gmail.com>
2025-03-28 09:35:14 -0300

    Testando Id

--- Program.cs
+++ Program.cs
@@ -1,1 +1,7 @@
-﻿
+﻿using ConcessionariCLI.Models;
+
+Manager one = new("Romualdo", 123456789, "Gerente");
+
+Console.WriteLine(one.Id);
+Manager two = new("Vasco", 1457, "Gerente");
+Console.WriteLine(two.Id);
