f68d01fb558f5984765679a4517ba38fb4397238
GabrielNP96 <gabrielnp1996@gmail.com>
2025-03-28 13:45:05 -0300

    nova clase

---
+++ Models/Seller.cs
@@ -0,0 +1,9 @@
+using ConcessionariaCLI.Enums;
+using ConcessionariaCLI.Models;
+class Seller : User
+{
+
+    public Seller(string name, string userName, string password, Position position):base (name, userName, password, position)
+    {}
+
+}
